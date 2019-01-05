be5cd7b492c8523dff03e9ac7df101f97c3a1aeb
Oleg Kleiman <oleg_kleyman@hotmail.com>
2019-01-05 21:27:42 +0200

    Initial Commit

---
+++ odette/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  odette
+//
+//  Created by Oleg Kleiman on 05/01/2019.
+//  Copyright © 2019 Oleg Kleiman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
