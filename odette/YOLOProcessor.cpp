df12f5cb0cfe05c98817d424b840302231a5c721
Oleg Kleiman <oleg_kleyman@hotmail.com>
2019-01-11 03:26:40 +0200

    Add files via upload

--- odette/YOLOProcessor.cpp
+++ odette/YOLOProcessor.cpp
@@ -52,45 +52,8 @@ void YOLOProcessor::process(Mat frame, int nCurrentFrame,
     
     vector<Mat> outs;
     m_net.forward(outs, getOutputsNames(m_net));
-
     postprocess(frame, outs);
 
-//    Mat detectionMat = m_net.forward();//outs[2]);
-//    for (int i = 0; i < detectionMat.rows; i++) {
-//        const int probability_index = 5;
-//        const int probability_size = detectionMat.cols - probability_index;
-//        float *prob_array_ptr = &detectionMat.at<float>(i, probability_index);
-//        size_t objectClass = max_element(prob_array_ptr, prob_array_ptr + probability_size) - prob_array_ptr;
-//        float confidence = detectionMat.at<float>(i, (int)objectClass + probability_index);
-//        if (confidence > m_confidenceThreshold) {
-//            float x_center = detectionMat.at<float>(i, 0) * frame.cols;
-//            float y_center = detectionMat.at<float>(i, 1) * frame.rows;
-//            float width = detectionMat.at<float>(i, 2) * frame.cols;
-//            float height = detectionMat.at<float>(i, 3) * frame.rows;
-//            Point p1(cvRound(x_center - width / 2), cvRound(y_center - height / 2));
-//            Point p2(cvRound(x_center + width / 2), cvRound(y_center + height / 2));
-//            Rect object(p1, p2);
-//
-////            Scalar object_roi_color(0, 255, 0);
-////            if (object_roi_style == "box")
-////            {
-////                rectangle(frame, object, object_roi_color);
-////            }
-////            else
-////            {
-////                Point p_center(cvRound(x_center), cvRound(y_center));
-////                line(frame, object.tl(), p_center, object_roi_color, 1);
-////            }
-////            String className = objectClass < m_classNamesVec.size() ? m_classNamesVec[objectClass] : cv::format("unknown(%d)", objectClass);
-////            String label = format("%s: %.2f", className.c_str(), confidence);
-////            int baseLine = 0;
-////            Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
-////            rectangle(frame, Rect(p1, Size(labelSize.width, labelSize.height + baseLine)),
-////                      object_roi_color, FILLED);
-////            putText(frame, label, p1 + Point(0, labelSize.height),
-////                    FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 0));
-//        }
-//    }
 }
 
 void YOLOProcessor::postprocess(Mat& frame, const vector<Mat>& outs) {
